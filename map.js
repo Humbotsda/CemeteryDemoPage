6a9a7500dd8ad6aca55011e9545f1deb5a24078a
azuspan607 <aa.zuspan@gmail.com>
2019-08-19 21:45:22 -0700

    Add ortho and basemap tiles, layer control, and automatic popup on search

--- map.js
+++ map.js
@@ -1,10 +1,4 @@
-var myMap = L.map('pointMap').setView([40.8796, -123.982], 19)
 
-
-var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
 
 
 var blueIcon = L.icon({
@@ -22,7 +16,44 @@ var goldIcon = L.icon({
 	iconUrl: "assets/icons/grave_icon_gold.png"
 })
 
-// Assign the correct icon based on the plot availability
+var home = {
+	lat: 40.87955,
+	lng: -123.9823,
+	zoom: 19
+} 
+
+// Create the map
+var myMap = L.map('pointMap').setView([home.lat, home.lng], home.zoom)
+
+// Add home button
+L.easyButton('fa-home', function(btn, map){
+	map.setView([home.lat, home.lng], home.zoom);
+	  },'Zoom To Home', {position:'topright'}).addTo(myMap);
+	
+// Reposition the zoom control
+myMap.zoomControl.setPosition('topright');
+	
+// Add a fullscreen toggle
+myMap.addControl(new L.Control.Fullscreen({position: 'topright'}));
+
+// Add basemap
+var tileCartoDBVoyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+		subdomains: 'abcd',
+		maxZoom: 25,
+		minZoom: 12
+	}).addTo(myMap);
+
+// Add orthophoto tileset
+var tileOrtho = L.tileLayer('https://api.mapbox.com/v4/{tilesetId}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 25,
+      minZoom: 15,
+      accessToken: 'pk.eyJ1IjoiaXNpbG1lMSIsImEiOiJjanR3amZvOW8yOHVzM3ltc2x3b3BibmtwIn0.St3CYo8jaThhr_HrV1QXdQ',
+      tilesetId: 'isilme1.bhbathwp'
+    }).addTo(myMap);
+
+// Assign the correct icon to grave plot points based on the plot availability
 function assignIcon(feature, layer)
 {
 	if (feature.properties.Status_Des == "Occupied")
@@ -39,6 +70,9 @@ function onEachFeature(feature, layer)
 	// Create a tooltip to show the current plot availability
 	layer.bindTooltip(feature.properties.Status_Des);
 	
+	layer.bindPopup(feature.properties.Name);
+
+	// Assign the correct icon
 	assignIcon(feature, layer);
 }
 
@@ -48,8 +82,42 @@ gravePoints = L.geoJSON(graveJSON, {
 	onEachFeature: onEachFeature
 }).addTo(myMap);
 
-var mySearch = new L.Control.Search({
+var graveSearch = new L.Control.Search({
 	layer: gravePoints,
 	propertyName: 'Name',
-	collapsed: false
+	collapsed: false,
+	textPlaceholder: 'Search by name...',
+	// When a location is found, fly there slowly
+	moveToLocation: function(latlng, title, map) {
+		var zoom =  22 
+			map.flyTo(latlng, zoom, {animate: true, duration: 3});
+	}
 }).addTo(myMap);
+
+// When a grave is searched for and found
+graveSearch.on('search:locationfound', function(e)
+{
+	// Open its popup automatically
+	e.layer.openPopup().openOn(myMap);
+})
+
+// Set up the layers for layer control
+var layerControl = {
+	base_layers: {},
+	overlays: {
+		"Street basemap" : tileCartoDBVoyager,
+		"Orthoimagery" : tileOrtho,
+		"Grave plots" : gravePoints
+	}
+}
+
+L.control.layers(
+	layerControl.base_layers,
+	layerControl.overlays,
+	{
+		"autoZIndex": true,
+		"collapsed": false,
+		"position": "topleft"
+	}
+).addTo(myMap);
+
