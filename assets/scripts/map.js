b82a814b30d555ca53d2f0743c8781d5e72b84ba
azuspan607 <aa.zuspan@gmail.com>
2019-08-20 00:09:07 -0700

    Split icons into template file

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -1,24 +1,3 @@
-var blueIcon = L.icon({
-	iconSize: [10, 10],
-	iconUrl: "assets/icons/grave_icon_blue.png"
-})
-
-var purpleIcon = L.icon({
-	iconSize: [10, 10],
-	iconUrl: "assets/icons/grave_icon_purple.png"
-})
-
-var goldIcon = L.icon({
-	iconSize: [10, 10],
-	iconUrl: "assets/icons/grave_icon_gold.png"
-})
-
-var home = {
-	lat: 40.87955,
-	lng: -123.9823,
-	zoom: 19
-} 
-
 // Create the map
 var myMap = L.map('pointMap').setView([home.lat, home.lng], home.zoom)
 
