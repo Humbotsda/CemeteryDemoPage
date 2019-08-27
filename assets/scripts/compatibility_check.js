bec7681f7275c77bbb026fe2b85d1305dfa79b94
azuspan607 <aa.zuspan@gmail.com>
2019-08-27 14:15:35 -0700

    Add ES6 compatibility check

---
+++ assets/scripts/compatibility_check.js
@@ -0,0 +1,14 @@
+// Run a test ES6 function and return true if it runs successfully
+function check() {
+    "use strict";
+
+    try { eval("var foo = (x)=>x+1"); }
+    catch (e) { return false; }
+    return true;
+}
+
+// If the browser fails to execute the ES6 code
+if (!check()) 
+{
+    alert('Your browser does not support modern Javascript ES6. Features of this site will not work. To use this site, please switch to a modern, compatible browser such as Chrome, Firefox, or Edge.')
+}
