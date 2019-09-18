68234e3c7103ab009bd138934e427bcc4062f966
azuspan607 <aa.zuspan@gmail.com>
2019-09-17 17:34:15 -0700

    Add ancestry link to video popup demo

--- assets/scripts/templates/popup_template.js
+++ assets/scripts/templates/popup_template.js
@@ -1,11 +1,10 @@
 // Return a popup table using the input feature properties
-function popupTemplate(feature)
-{
-    var name = feature.properties.Name;
-    var birth = feature.properties.Birth;
-    var death = feature.properties.Death;
+function popupTemplate(feature) {
+  var name = feature.properties.Name;
+  var birth = feature.properties.Birth;
+  var death = feature.properties.Death;
 
-    templateString = `
+  templateString = `
     <!DOCTYPE html>
     <html>
         <body>
@@ -25,25 +24,37 @@ function popupTemplate(feature)
             </table>
         </body>
     </html>
-    `
-    return templateString;
+    `;
+  return templateString;
 }
 
 // Return a popup table using the input feature properties
-function videoPopupTemplate(feature)
-{
-    var name = feature.properties.Name;
-    var birth = feature.properties.Birth;
-    var death = feature.properties.Death;
+function videoPopupTemplate(feature) {
+  var name = feature.properties.Name;
+  var birth = feature.properties.Birth;
+  var death = feature.properties.Death;
 
-    templateString = `
+  //  Try splitting the name into first and last
+  try {
+    var first_name = name.split(" ")[0];
+  } catch (err) {
+    // Leave the names empty if they aren't found
+    var first_name = "";
+  }
+  try {
+    var last_name = name.split(" ")[1];
+  } catch (err) {
+    var last_name = "";
+  }
+
+  templateString = `
     <!DOCTYPE html>
     <html>
         <body>
             <table id="t01">
                 <tr>
                     <td colspan="2">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/wg3ywGdyCm8" frameborder="0" 
+                        <iframe width="300" height="200" src="https://www.youtube.com/embed/wg3ywGdyCm8" frameborder="0" 
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
                         </iframe>
                     </td>
@@ -60,9 +71,16 @@ function videoPopupTemplate(feature)
                     <td><b>Death year</b></td>
                     <td>${death}</td>
                 </tr>
+                <tr>
+                    <td colspan="2" class="table-button-row">
+                        <a href="https://www.ancestry.com/search/?name=${first_name}_${last_name}&birth=${birth}" target="_blank">
+                            <button>Find ${name} on Ancestry.com</button>
+                        </a>
+                    </td>
+                </tr>
             </table>
         </body>
     </html>
-    `
-    return templateString;
+    `;
+  return templateString;
 }
