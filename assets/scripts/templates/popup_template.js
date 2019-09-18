991bc5d4b5cba94e65df345545de9a0dfa35ab2a
azuspan607 <aa.zuspan@gmail.com>
2019-09-18 14:07:54 -0700

    Refactor template html to remove redundant tags

--- assets/scripts/templates/popup_template.js
+++ assets/scripts/templates/popup_template.js
@@ -5,25 +5,20 @@ function popupTemplate(feature) {
   var death = feature.properties.Death;
 
   templateString = `
-    <!DOCTYPE html>
-    <html>
-        <body>
-            <table id="t01">
-                <tr>
-                    <td><b>Name</b></td>
-                    <td>${name}</td>
-                </tr>
-                <tr>
-                    <td><b>Birth year</b></td>
-                    <td>${birth}</td>
-                </tr>
-                <tr>
-                    <td><b>Death year</b></td>
-                    <td>${death}</td>
-                </tr>
-            </table>
-        </body>
-    </html>
+        <table class="popup-table">
+            <tr>
+                <th>Name</th>
+                <td>${name}</td>
+            </tr>
+            <tr>
+                <th>Birth year</th>
+                <td>${birth}</td>
+            </tr>
+            <tr>
+                <th>Death year</th>
+                <td>${death}</td>
+            </tr>
+        </table>
     `;
   return templateString;
 }
@@ -48,39 +43,34 @@ function videoPopupTemplate(feature) {
   }
 
   templateString = `
-    <!DOCTYPE html>
-    <html>
-        <body>
-            <table id="t01">
-                <tr>
-                    <td colspan="2">
-                        <iframe width="300" height="200" src="https://www.youtube.com/embed/wg3ywGdyCm8" frameborder="0" 
-                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                        </iframe>
-                    </td>
-                </tr>
-                <tr>
-                    <td><b>Name</b></td>
-                    <td>${name}</td>
-                </tr>
-                <tr>
-                    <td><b>Birth year</b></td>
-                    <td>${birth}</td>
-                </tr>
-                <tr>
-                    <td><b>Death year</b></td>
-                    <td>${death}</td>
-                </tr>
-                <tr>
-                    <td colspan="2" class="table-button-row">
-                        <a href="https://www.ancestry.com/search/?name=${first_name}_${last_name}&birth=${birth}" target="_blank">
-                            <button>Find ${name} on Ancestry.com</button>
-                        </a>
-                    </td>
-                </tr>
-            </table>
-        </body>
-    </html>
+        <table class="popup-table">
+            <tr>
+                <td colspan="2">
+                    <iframe width="300" height="200" src="https://www.youtube.com/embed/wg3ywGdyCm8" frameborder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                    </iframe>
+                </td>
+            </tr>
+            <tr>
+                <th>Name</th>
+                <td>${name}</td>
+            </tr>
+            <tr>
+                <th>Birth year</th>
+                <td>${birth}</td>
+            </tr>
+            <tr>
+                <th>Death year</th>
+                <td>${death}</td>
+            </tr>
+            <tr>
+                <td colspan="2" class="table-button-row">
+                    <a href="https://www.ancestry.com/search/?name=${first_name}_${last_name}&birth=${birth}" target="_blank">
+                        <button>Find ${name} on Ancestry.com</button>
+                    </a>
+                </td>
+            </tr>
+        </table>
     `;
   return templateString;
 }
